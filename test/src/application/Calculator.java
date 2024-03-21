030d429bc4a1e6a7f97d1578eea415fa399a9c13
EP-Z790\admin <reolch@gmail.com>
2024-03-21 19:56:39 +0900

    不要なコードを削除しました。

--- test/src/application/Calculator.java
+++ test/src/application/Calculator.java
@@ -2,12 +2,9 @@ package application;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Calculator extends Application {
@@ -16,42 +13,7 @@ public class Calculator extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // ラベルを作成する
-        Label label = new Label("Calculator");
-        // ラベルの優先幅を100にする
-        label.setPrefWidth(100); // 注1
-        // テキストフィールドを作成する
-        TextField textField = new TextField("0");
-        // テキストフィールドの優先桁数を20に設定する
-        textField.setPrefColumnCount(20); // 注2
-        // 計算ボタンの記号を準備する
-        String[] operator = { "+", "-", "\u00D7", "\u00F7", "=" }; // ＋－×÷＝
-        // 5個の計算ボタンを作成する
-        Button[] button = new Button[5];
-        for (int i = 0; i < 5; i++) {
-            // ボタンをインスタンス化する
-            button[i] = new Button(operator[i]);
-            //  ボタンのアクションを登録する
-            button[i].setOnAction(event -> calculate(event, textField));
-            // ボタンの優先幅を30に設定する
-            button[i].setPrefWidth(30); // 注3
-        }
-        // 直前に押されたボタンを表示するラベルを作成する
-        pressed = new Label();
-        // HBoxペインを作成し、２つのラベルを配置する
-        HBox hbox1 = new HBox(label, pressed);
-        // HBoxペインを作成し、計算ボタンを配置する
-        HBox hbox2 = new HBox(button);
-        // VBoxペインを作成し、作成した部品を配置する
-        VBox vbox = new VBox(hbox1, textField, hbox2);
-        // シーンを作成し、ペインに入れる
-        Scene scene = new Scene(vbox);
-        // ステージにVBoxペインを入れる
-        primaryStage.setScene(scene);
-        // ステージのタイトルバーを設定する
-        primaryStage.setTitle("Calculator");
-        // ステージを表示する
-        primaryStage.show();
+
     }
 
     // 計算をする
